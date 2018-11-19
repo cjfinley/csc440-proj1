b44dea195578f5743593e5f7d7c969e30f9653c6
eadunlop <eadunlop@ncsu.edu>
2018-11-18 22:52:54 -0500

    implemented setter methods for individual fields in Employee tables, and an updateEmployee method

--- 440Project12018/src/project1/CSC440/Fall2018/user/Employee.java
+++ 440Project12018/src/project1/CSC440/Fall2018/user/Employee.java
@@ -14,40 +14,65 @@ public interface Employee {
 		fields[6] = compensation;
 		try{
 			st.executeUpdate("INSERT INTO Employee", fields);
-		} catch( Exception e ){
-			System.err.println("Error creating employee: ");
-			System.err.println(e.getMessage());
-		}	
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error creating employee");
+		}
 	}
 	static ResultSet getEmployee(Statement st, String eid){
 		String qstr = "SELECT * FROM Employee WHERE eid = " + eid;
 		ResultSet rs = null;
 		try{
 			rs = st.executeQuery(qstr);
-		} catch(Exception e){
-			System.err.println("Error retrieving employee: ");
-			System.err.println(e.getMessage());
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error getting employee");
 		}
 		return rs;
+	}
+	static void updateEmployee(Statement st, String eid, String name, String address, String email, String phoneNum, String startDate, String compensation){
+		String qstr = "UPDATE Employee SET";
+		if (!(name == null) && !(name.length() == 0)){
+			qstr += " name = " + name + ",";
+		}
+		if (!(address == null) && !(address.length() == 0)){
+			qstr += " address = " + address + ",";
+		}
+		if (!(email == null) && !(email.length() == 0)){
+			qstr += " email = " + email + ",";
+		}
+		if (!(phoneNum == null) && !(phoneNum.length() == 0)){
+			qstr += " phone = " + phoneNum + ",";
+		}
+		if (!(startDate == null) && !(startDate.length() == 0)){
+			qstr += " startDate = " + startDate + ",";
+		}
+		if (!(compensation == null) && !(compensation.length() == 0)){
+			qstr += " rate = " + compensation + ",";
+		}
+		
+		qstr = qstr.substring(0, qstr.length() - 1);
+		qstr += " WHERE eid = " + eid;
+		try{
+			st.executeUpdate(qstr);
+		} catch (SQLException e){
+			throw new IllegalArgumentException("Error updating employee");
+		}
 	}
 	static ResultSet getName(Statement st, String eid){
 		String qstr = "SELECT name FROM Employee WHERE eid = " + eid;
 		ResultSet rs = null;
 		try{
 			rs = st.executeQuery(qstr);
-		} catch(Exception e){
-			System.err.println("Error retrieving employee name: ");
-			System.err.println(e.getMessage());
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error getting name");
 		}
 		return rs;
 	}
 	static void setName(Statement st, String eid, String name){
-		String qstr = "UPDATE t SET t.name = " + name + " FROM Employee t WHERE eid = " + eid;
+		String qstr = "UPDATE Employee SET name = " + name + " WHERE eid = " + eid;
 		try{
 			st.executeUpdate(qstr);
-		} catch (Exception e){
-			System.err.println("Error setting employee name: ");
-			System.err.println(e.getMessage());
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error setting name");
 		}
 	}
 	static ResultSet getAddress(Statement st, String eid){
@@ -55,69 +80,89 @@ public interface Employee {
 		ResultSet rs = null;
 		try{
 			rs = st.executeQuery(qstr);
-		} catch(Exception e){
-			System.err.println("Error retrieving employee address: ");
-			System.err.println(e.getMessage());
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error getting email");
 		}
 		return rs;
 	}
-	static void setAddress(Statement st, String eid){
-		
+	static void setAddress(Statement st, String eid, String address){
+		String qstr = "UPDATE Employee SET address = " + address + " WHERE eid = " + eid;
+		try{
+			st.executeUpdate(qstr);
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error setting address");
+		}
 	}
 	static ResultSet getEmail(Statement st, String eid){
 		String qstr = "SELECT email FROM Employee WHERE eid = " + eid;
 		ResultSet rs = null;
 		try{
 			rs = st.executeQuery(qstr);
-		} catch(Exception e){
-			System.err.println("Error retrieving employee email: ");
-			System.err.println(e.getMessage());
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error getting email");
 		}
 		return rs;
 	}
-	static void setEmail(Statement st, String eid){
-		
+	static void setEmail(Statement st, String eid, String email){
+		String qstr = "UPDATE Employee SET email = " + email + " WHERE eid = " + eid;
+		try{
+			st.executeUpdate(qstr);
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error setting email");
+		}
 	}
 	static ResultSet getPhoneNum(Statement st, String eid){
 		String qstr = "SELECT phone FROM Employee WHERE eid = " + eid;
 		ResultSet rs = null;
 		try{
 			rs = st.executeQuery(qstr);
-		} catch(Exception e){
-			System.err.println("Error retrieving employee phone number: ");
-			System.err.println(e.getMessage());
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error getting phone number");
 		}
 		return rs;
 	}
-	static void setPhoneNum(Statement st, String eid){
-		
+	static void setPhoneNum(Statement st, String eid, String phone){
+		String qstr = "UPDATE Employee SET phone = " + phone + " WHERE eid = " + eid;
+		try{
+			st.executeUpdate(qstr);
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error setting phone number");
+		}
 	}
 	static ResultSet startDate(Statement st, String eid){
 		String qstr = "SELECT startDate FROM Employee WHERE eid = " + eid;
 		ResultSet rs = null;
 		try{
 			rs = st.executeQuery(qstr);
-		} catch(Exception e){
-			System.err.println("Error retrieving employee start date: ");
-			System.err.println(e.getMessage());
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error getting start date");
 		}
 		return rs;
 	}
-	static void setStartDate(Statement st, String eid){
-		
+	static void setStartDate(Statement st, String eid, String startDate){
+		String qstr = "UPDATE Employee SET startDate = " + startDate + " WHERE eid = " + eid;
+		try{
+			st.executeUpdate(qstr);
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error setting start date");
+		}
 	}
 	static ResultSet getComp(Statement st, String eid){
 		String qstr = "SELECT rate FROM Employee WHERE eid = " + eid;
 		ResultSet rs = null;
 		try{
 			rs = st.executeQuery(qstr);
-		} catch(Exception e){
-			System.err.println("Error retrieving employee compensation rate: ");
-			System.err.println(e.getMessage());
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error getting compensation rate");
 		}
 		return rs;
 	}
-	static void setComp(Statement st, String eid){
-		
+	static void setComp(Statement st, String eid, String rate){
+		String qstr = "UPDATE Employee SET rate = " + rate + " WHERE eid = " + eid;
+		try{
+			st.executeUpdate(qstr);
+		} catch (SQLException e) {
+			throw new IllegalArgumentException("Error setting compensation rate");
+		}
 	}
 }
